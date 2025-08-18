6faae39cb502d1856fe9baecf4be008233720361
topus <topus@users.noreply.github.com>
2025-08-18 22:29:39 +0200

    Upgrade Chapter03 from JUnit 4 to JUnit 5 and add Claude Code gitignore…

--- Chapter03/src/test/java/dev/davivieira/RunCucumberTest.java
+++ Chapter03/src/test/java/dev/davivieira/RunCucumberTest.java
@@ -1,11 +1,18 @@
 package dev.davivieira;
 
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber-result"}, features = "src/test/java/dev/davivieira/features")
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectPackages("dev.davivieira")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "dev.davivieira")
 public class RunCucumberTest {
 }
